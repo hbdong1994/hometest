2baf500b972f06535494afcb2ab67c38f4ffd78d
deandong <844199574@qq.com>
2018-01-09 00:14:54 +0800

    add bootstrap in vote

--- vote/index.php
+++ vote/index.php
@@ -1,2 +1,8 @@
 <?php
+$cfg = require('config.php');
+
+$auth_url = "https://zjuam.zju.edu.cn/cas/oauth2.0/authorize";
+$auth_url .= "?response_type=code&client_id={$cfg['app_secret']}&redirect_uro={$cfg['redirect_uri']}";
+
+header('location:'.$auth_url);
 
